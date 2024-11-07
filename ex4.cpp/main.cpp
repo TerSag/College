2c54d8ffa072ccc45bb8aac8e3ba0d1fe2de2725
TerSag <sashka.tereshuk@gmail.com>
2024-11-07 23:51:36 +0200

    C++Lab1.

--- ex4.cpp/main.cpp
+++ ex4.cpp/main.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
 
+
+int main () 
+{
+   double A,B,C;
+
+   std::cout << "Введіть довжини сторин паралелепіпеда (A,B,C):";
+   std::cin >> A >> B >> C;
+
+   if (A > 0 && B > 0 && C > 0 )
+  {
+    std::cout << "Об'єм паралелепіпеда:" << A * B * C << std::endl;
+    std::cout << "Площа поверхні паралелепіпеда:" << 2 * ( A * B + B * C + A * C) << std::endl;
+  
+   }
+
+
+return 0;
+}
--- ex6.cpp/main.cpp
+++ ex6.cpp/main.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
+#include <iomanip>
 
+int main () 
+{
+
+   float P , R;
+   int T;
+
+   std::cout << "Введіть суму позики: ";
+   std::cin >> P;
+   std::cout << "Введіть період позики: ";
+   std::cin >> T;
+   std::cout << "Введіть процентну ставку: ";
+   std::cin >> R;
+
+   float I = (P * T * R) / 100;
+
+   std::cout << std::fixed << std::setprecision(2);
+   std::cout << "Відсоток (float): " << I << std::endl;
+   std::cout << "Відсоток (int): " << static_cast<int>(I) << std::endl;
+
+   
+return 0;
+}
